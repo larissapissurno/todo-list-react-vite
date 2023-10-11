0d1849333e0d29fadfd952d47b516d101b081777
Larissa Pissurno <larissapissurno@gmail.com>
2023-10-11 12:05:30 -0300

    cria um estado para guardar o texto informado no input

--- src/components/AddTodoInput.jsx
+++ src/components/AddTodoInput.jsx
@@ -1,9 +1,15 @@
+import React, { useState } from "react";
 import { Button, Container, TextInput } from "./AddTodoInput.styled";
 
 export function AddTodoInput() {
+  const [description, setDescription] = useState("");
+
   return (
     <Container>
-      <TextInput placeholder="Adicione uma nova tarefa..." />
+      <TextInput
+        placeholder="Adicione uma nova tarefa..."
+        onChange={(event) => setDescription(event.target.value)}
+      />
       <Button>Add</Button>
     </Container>
   );
